9111929189afee8a4884c6d176dd2c1d3e7689f7
hczerpak <hczerpak@theworkshop.com>
2018-12-04 07:20:01 +0000

    Setting up spring boot java project instead

--- src/main/java/com/jvmp/vouchershop/domain/Voucher.java
+++ src/main/java/com/jvmp/vouchershop/domain/Voucher.java
@@ -1,23 +1,18 @@
 package com.jvmp.vouchershop.domain;
 
+import lombok.Builder;
+import lombok.Value;
 
 import java.time.LocalDate;
 
+@Value
+@Builder
 public class Voucher {
-
     public final long amount;
     public final String currency;
     public final String id;
+    public final boolean published;
     public final boolean redeemed;
     public final long serialNumber;
     public final LocalDate useBy;
-
-    public Voucher(long amount, String currency, String id, boolean redeemed, long serialNumber, LocalDate useBy) {
-        this.amount = amount;
-        this.currency = currency;
-        this.id = id;
-        this.redeemed = redeemed;
-        this.serialNumber = serialNumber;
-        this.useBy = useBy;
-    }
 }
