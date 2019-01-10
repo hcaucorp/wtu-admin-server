dca45f7711ba5751cadc2029e42fcab6c59858b8
Hubert Czerpak <hubert.czerpak@gmail.com>
2019-01-10 18:28:57 +0000

    added JWT token verification

--- src/main/java/com/jvmp/vouchershop/Application.java
+++ src/main/java/com/jvmp/vouchershop/Application.java
@@ -1,6 +1,5 @@
 package com.jvmp.vouchershop;
 
-import org.bitcoinj.utils.BriefLogFormatter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -10,8 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class Application {
 
     public static void main(String[] args) {
-        BriefLogFormatter.init();
-
         SpringApplication.run(Application.class, args);
     }
 }
