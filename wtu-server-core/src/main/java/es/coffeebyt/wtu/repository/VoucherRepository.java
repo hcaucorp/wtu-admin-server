fa1a1c537e35f8315d30f91c010358ff8b86009c
hczerpak <hczerpak@theworkshop.com>
2019-09-07 19:13:44 +0200

    Minor refactoring. Adding new methods, sorting old ones.

--- wtu-server-core/src/main/java/es/coffeebyt/wtu/repository/VoucherRepository.java
+++ wtu-server-core/src/main/java/es/coffeebyt/wtu/repository/VoucherRepository.java
@@ -10,19 +10,21 @@ import java.util.Optional;
 @Repository
 public interface VoucherRepository extends JpaRepository<Voucher, Long> {
 
+    Optional<Voucher> findByCode(String code);
+
+    List<Voucher> findBySoldTrue();
+    List<Voucher> findBySoldFalse();
     List<Voucher> findBySoldFalseAndSku(String sku);
 
+    List<Voucher> findByPublishedFalse();
     List<Voucher> findByPublishedFalseAndSku(String sku);
-
+    List<Voucher> findByPublishedTrue();
     List<Voucher> findByPublishedTrueAndSoldFalseAndSku(String sku);
 
     List<Voucher> findByRedeemedFalse();
-
-    List<Voucher> findByRedeemedTrue();
-
     List<Voucher> findByRedeemedFalseAndSku(String sku);
 
+    List<Voucher> findByRedeemedTrue();
     List<Voucher> findByRedeemedTrueAndSku(String sku);
 
-    Optional<Voucher> findByCode(String code);
 }
